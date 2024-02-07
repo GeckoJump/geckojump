f44e4d25344922c62a5583fa1faaa827a2c0180b
Hunter Goram <huntergoram2@gmail.com>
2024-02-07 15:47:16 -0500

    Add specific fonts for software section, align icon with subheading text, and added drafts for descriptions.

--- frontend/src/components/SoftwareServiceItem.tsx
+++ frontend/src/components/SoftwareServiceItem.tsx
@@ -9,11 +9,11 @@ interface SoftwareServiceItemProps {
 export const SoftwareServiceItem = ({ title, description, icon }: SoftwareServiceItemProps) => {
   return (
     <div className="flex flex-col align-baseline">
-      <h3 className="flex flex-row text-xl font-sans font-bold p-3 pb-2 gap-2 lg:justify-start justify-center">
+      <h3 className="flex flex-row text-xl leading-snug font-sans font-bold p-3 pl-0 gap-2 lg:justify-start justify-center">
         <div className="text-2xl my-auto">{icon}</div>
         {title}
       </h3>
-      <p className="text-gray-500 w-3/4 lg:text-left lg:mx-0 text-center mx-auto">{description}</p>
+      <p className="text-gray-500 w-full lg:text-left lg:mx-0 leading-relaxed text-center mx-auto">{description}</p>
     </div>
   )
 }
--- frontend/src/components/SoftwareServices.tsx
+++ frontend/src/components/SoftwareServices.tsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { SoftwareServiceItem } from './SoftwareServiceItem'
 import { FaCheckCircle } from 'react-icons/fa'
+import { GoGoal } from "react-icons/go";
+import { IoIosPeople } from "react-icons/io";
 
 export const SoftwareServices = () => {
   return (
     <div className="flex flex-col align-baseline">
-      <h2 className="text-3xl font-sans font-bold pb-2 text-center">Software Services</h2>
-      <p className="text-gray-500 text-center mb-10">We offer a variety of software services to help you get started with your project.</p>
+      <h2 className="text-4xl font-bold pb-2 text-center font-BebasNeue">Software <span className='text-emerald-700'>For You</span></h2>
+      <p className="text-gray-500 text-center mb-10">Empowering your Vision with customized software solutions crafted specifically for your unique needs.</p>
       <SoftwareServiceItem 
-        title="Web Development"
-        description="We can help you build a website that will help you succeed. Our team has the expertise to provide the best solutions for your business."
-        icon={<FaCheckCircle size={16} />} 
+        title="Let's Talk About Your Goals"
+        description="We start with personalized conversations to grasp your unique business goals. This way, our software solutions are perfectly aligned with your vision for success."
+        icon={<GoGoal size={20} />} 
       />
       <SoftwareServiceItem 
-        title="Web Development"
-        description="We can help you build a website that will help you succeed. Our team has the expertise to provide the best solutions for your business."
-        icon={<FaCheckCircle size={16} />} 
+        title="We're in This Together"
+        description="We foster ongoing collaboration, evolving our solutions alongside your business needs. Our commitment to regular communication ensures that our software remains a dynamic asset for your sustained success."
+        icon={<IoIosPeople size={24} />} 
       />
     </div>
   )
