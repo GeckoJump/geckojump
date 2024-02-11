a4ae159219f6c5500d48de1b7b52b025f3276760
Hunter Goram <huntergoram2@gmail.com>
2024-02-10 21:06:14 -0500

    Add scroll utils, hamburger menu on small devices, smooth scroll to sections in navbar.

--- frontend/src/components/ServiceItem.tsx
+++ frontend/src/components/ServiceItem.tsx
@@ -10,7 +10,6 @@ export const ServiceItem = ({ title, description, icon }: SoftwareServiceItemPro
   return (
     <div className="flex flex-col align-baseline">
       <h3 className="flex flex-row text-xl leading-snug font-sans font-bold p-3 pl-0 gap-2 lg:justify-start justify-center">
-        <div className="text-2xl my-auto">{icon}</div>
         {title}
       </h3>
       <p className="text-gray-500 w-full lg:text-left lg:mx-0 leading-relaxed text-center mx-auto">{description}</p>
