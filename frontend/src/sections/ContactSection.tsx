349939697c264f758d29ab4776c503b52318cefb
Hunter Goram <huntergoram2@gmail.com>
2024-02-12 21:45:39 -0500

    Misc contact section styling tweak.

--- frontend/src/sections/ContactSection.tsx
+++ frontend/src/sections/ContactSection.tsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { FaPaperPlane } from 'react-icons/fa';
+import { GoPaperAirplane } from 'react-icons/go';
+import { IoIosAirplane, IoMdPaperPlane } from 'react-icons/io';
 
 export const ContactSection = () => {
   const [name, setName] = React.useState('');
@@ -47,14 +50,14 @@ export const ContactSection = () => {
       <h2 className="text-4xl font-bold pb-2 text-center font-BebasNeue">Let's Get <span className="text-emerald-700">To Work</span></h2>
       <p className="text-gray-500 px-4 md:px-0 text-center mb-10">We would love to work with you. Leave us your contact information and we will be in touch as soon as possible. </p>
       <form onSubmit={handleSubmit} className="flex flex-col w-3/4 lg:w-1/2 gap-4 justify-center">
-        <input onChange={(e) => setName(e.target.value)} className="p-3 rounded-lg outline-2 hover:outline focus:outline outline-emerald-700/50 transition-colors duration-300 shadow-md" type="text" placeholder="Your Name" />
-        <input onChange={(e) => setEmail(e.target.value)} className="p-3 rounded-lg outline-2 hover:outline focus:outline outline-emerald-700/50 transition-colors duration-300 shadow-md" type="email" placeholder="Your Email" />
+        <input onChange={(e) => setName(e.target.value)} value={name} className="p-3 rounded-lg outline-2 hover:outline focus:outline outline-emerald-700/50 transition-colors duration-300 shadow-md" type="text" placeholder="Your Name" />
+        <input onChange={(e) => setEmail(e.target.value)} value={email} className="p-3 rounded-lg outline-2 hover:outline focus:outline outline-emerald-700/50 transition-colors duration-300 shadow-md" type="email" placeholder="Your Email" />
         <div className="flex flex-col md:flex-row w-full gap-4">
-          <input onChange={(e) => setPhone(e.target.value)} className="p-3 flex-1 rounded-lg outline-2 hover:outline focus:outline outline-emerald-700/50 transition-colors duration-300 shadow-md" type="text" placeholder="Your Phone" />
-          <input onChange={(e) => setCompany(e.target.value)} className="p-3 flex-1 rounded-lg outline-2 hover:outline focus:outline outline-emerald-700/50 transition-colors duration-300 shadow-md" type="text" placeholder="Your Company Name" />
+          <input onChange={(e) => setPhone(e.target.value)} value={phone} className="p-3 flex-1 rounded-lg outline-2 hover:outline focus:outline outline-emerald-700/50 transition-colors duration-300 shadow-md" type="text" placeholder="Your Phone" />
+          <input onChange={(e) => setCompany(e.target.value)} value={company} className="p-3 flex-1 rounded-lg outline-2 hover:outline focus:outline outline-emerald-700/50 transition-colors duration-300 shadow-md" type="text" placeholder="Your Company Name" />
         </div>
-        <textarea onChange={(e) => setMessage(e.target.value)} className="p-3 h-36 rounded-lg outline-2 hover:outline focus:outline outline-emerald-700/50 transition-colors duration-300 shadow-md" placeholder="Additional information"></textarea>
-        <button className="p-3 bg-emerald-700 hover:bg-emerald-500 w-1/4 self-start text-white rounded-lg shadow-2xl hover:shadow-slate-400 transition-all duration-150">{isSubmitting ? "Sending..." : "Send"}</button>
+        <textarea onChange={(e) => setMessage(e.target.value)} value={message} className="p-3 h-36 rounded-lg outline-2 hover:outline focus:outline outline-emerald-700/50 transition-colors duration-300 shadow-md" placeholder="Additional information"></textarea>
+        <button className="p-2 bg-emerald-700 font-semibold hover:bg-emerald-500 w-1/5 self-start text-white rounded-lg shadow-2xl hover:shadow-slate-400 transition-all duration-150">{isSubmitting ? "Sending..." : (<div className='flex flex-row justify-center gap-1 items-baseline'>Send<IoMdPaperPlane className='relative top-0.5' size={16}  /></div>)}</button>
       </form>
     </div>
   )
