2eb7b366ceb71e02a8b764c6ae68edd646cf51ad
Hunter Goram <huntergoram2@gmail.com>
2024-03-05 09:29:56 -0500

    Remove misc logs.

--- frontend/src/components/TwoSidedLayout.tsx
+++ frontend/src/components/TwoSidedLayout.tsx
@@ -13,8 +13,6 @@ export const TwoSidedLayout = ({ children, additionalClasses }: TwoSidedLayoutPr
     triggerOnce: true
   })
 
-  console.log(additionalClasses)
-
   return (
     <div ref={ref} className={'grid items-center md:grid-cols-2 grid-cols-1 lg:w-3/5 md:w-4/5 w-[90%] mx-auto gap-10 ' + (additionalClasses ?? '') }>
       <div className={`transition-all duration-1000 ${inView ? 'opacity-100 translate-x-0' : 'opacity-0 -translate-x-4'}`}>
