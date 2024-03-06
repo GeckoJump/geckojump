fe53bce2e2e4e875db09a9118a92f7c2aae22554
Hunter Goram <huntergoram2@gmail.com>
2024-03-06 17:21:31 -0500

    Fix logout not logging out users on backend.

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { ScrollLink } from './ScrollLink'
 
+const LINK_CLASS = 'text-md text-zinc-50 tracking-wide hover:text-zinc-200 transition-all duration-300 ease-in-out'
 
 export const Footer = () => {
   return (
     <div className="flex justify-center w-full bg-background py-10">
-      <div className="container flex flex-col w-3/5">
+      <div className="container flex flex-col w-3/4 lg:w-1/2">
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-          <ScrollLink to="hero-section" className="text-lg font-semibold text-white" disableUnderline>Home</ScrollLink>
-          <ScrollLink to="values-section" className="text-lg font-semibold text-white" disableUnderline>Our Values</ScrollLink>
-          <ScrollLink to="software-services-section" className="text-lg font-semibold text-white" disableUnderline>Software Services</ScrollLink>
-          <ScrollLink to="consulting-services-section" className="text-lg font-semibold text-white" disableUnderline>Consulting Services</ScrollLink>
-          <ScrollLink to="contact-section" className="text-lg font-semibold text-white" disableUnderline>Contact</ScrollLink>
+          <ScrollLink to="hero-section" className={LINK_CLASS} disableUnderline>Home</ScrollLink>
+          <ScrollLink to="values-section" className={LINK_CLASS} disableUnderline>Our Values</ScrollLink>
+          <ScrollLink to="software-services-section" className={LINK_CLASS} disableUnderline>Software Services</ScrollLink>
+          <ScrollLink to="consulting-services-section" className={LINK_CLASS} disableUnderline>Consulting Services</ScrollLink>
+          <ScrollLink to="contact-section" className={LINK_CLASS} disableUnderline>Contact</ScrollLink>
 
         </div>
         <div className='divider w-full my-4 bg-gray-500'></div>
